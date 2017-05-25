f3bcd75ff7f9830abf2de9897f66f0b842c64b91
Zachary Schreiber <zachgives@gmail.com>
2017-05-25 11:22:26 -0400

    comments

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,14 @@ class App extends React.Component {
 
    }
 
+
+//Used Object.keys() to create key values for each object. Then ran Object.map().
+//Using a facy es6 arrow fuction, I use the key value as an argument to reference each object.
+//I pass the data from each object to the Tablerow component with the details attribute.
+//The data is passed using React 'props'.
+//Also considered using the forEach() method, but this worked.
+
+
   render() {
     return (
       <div>
